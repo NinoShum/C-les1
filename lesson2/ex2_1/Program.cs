dc84e64d877bd1ce646ecf7d05a4df4f5e718c77
Шумайлова Нина <ninashum@gmail.com>
2022-10-15 14:35:57 +0300

    Lesson2

---
+++ lesson2/ex2_1/Program.cs
@@ -0,0 +1,21 @@
+﻿int a1 = 10;
+int a2 = 15;
+int a3 = 520;
+int a4 = 60;
+int a5 = 1;
+int a6 = 20;
+int a7 = 1;
+int a8 = -20;
+int a9 = 10;
+
+int max = a1;
+if (a2 > max) max = a2;
+if (a3 > max) max = a3;
+if (a4 > max) max = a4;
+if (a5 > max) max = a5;
+if (a6 > max) max = a6;
+if (a7 > max) max = a7;
+if (a8 > max) max = a8;
+if (a9 > max) max = a9;
+
+Console.WriteLine (max);
