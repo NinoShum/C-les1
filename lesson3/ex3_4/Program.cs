5fe22e5261d9042a23ae42c6c3d914e197685092
Шумайлова Нина <ninashum@gmail.com>
2022-10-22 16:18:37 +0300

    Lesson3-5

---
+++ lesson3/ex3_4/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Заменить пмробел черточкой /
+мальнькую к заменить на большую К
+С заменить на с
+*/
+
+string text = "-Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+
+// к типу строки применим способ, обращения к элементу по индексу
+// string s = "qwerty"
+//             012
+
+//s[3] ---r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if ((text[i]) == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string NewText = Replace(text, ' ', '|');
+Console.WriteLine(NewText);
+
+Console.WriteLine();
+NewText = Replace(Newtext, 'к', 'К');
+Console.WriteLine(NewText);
+
+Console.WriteLine();
+NewText = Replace(Newtext, 'С', 'с');
+Console.WriteLine(NewText);
