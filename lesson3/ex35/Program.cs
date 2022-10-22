864878cb6cd6f6eb09f957fb8d12ed8a64be05af
Шумайлова Нина <ninashum@gmail.com>
2022-10-22 16:54:07 +0300

    Lesson3-9

--- lesson3/ex35/Program.cs
+++ lesson3/ex35/Program.cs
@@ -1,4 +1,4 @@
-﻿// сортировка в массиве
+﻿// сортировка в массиве по возрастанию
 
 int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
 
@@ -36,3 +36,44 @@ void SelectionSort(int[] array)
 PrintArray(arr);
 SelectionSort(arr);
 PrintArray(arr);
+
+/*
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+
+} 
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int maxPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+
+        } 
+        
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
+*/
+
