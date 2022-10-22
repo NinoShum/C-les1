7ee1a8d6dbd668b98328422ebe001b3120f1b260
Шумайлова Нина <ninashum@gmail.com>
2022-10-22 16:48:37 +0300

    Lesson3-8

--- lesson3/ex35/Program.cs
+++ lesson3/ex35/Program.cs
@@ -14,7 +14,25 @@ void PrintArray(int[] array)
 
 } 
 
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+
+        } 
+        
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
 
 
-
+PrintArray(arr);
+SelectionSort(arr);
 PrintArray(arr);
