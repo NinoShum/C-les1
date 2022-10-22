69fb45644102883d5615b202f9a51e8daef57ee7
Шумайлова Нина <ninashum@gmail.com>
2022-10-22 15:43:27 +0300

    Lesson3-3

---
+++ lesson3/ex32_for/Program.cs
@@ -0,0 +1,15 @@
+﻿// For
+string Method4(int count,string Text)
+{
+    string result = String.Empty; //обозначает что строка пустая
+
+    for(int i = 0; i < count; i++)
+    {
+        result = result + Text;
+    }
+    return result;
+}
+
+//string res = Method4(count: 10, Text: "Oz");
+string res = Method4(5,"Ri");
+Console.WriteLine(res);
